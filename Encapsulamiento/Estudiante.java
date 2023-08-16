502924437ec9804d85ff992d9473425a84acbcdf
Mickey Guzman <111444662+MiguelUgalde16@users.noreply.github.com>
2023-08-15 23:29:41 -0600

    Tarea 1

---
+++ Encapsulamiento/Estudiante.java
@@ -0,0 +1,22 @@
+package Encapsulamiento;
+
+public class Estudiante {
+    //Atributos
+    private int id;
+    private String email;
+    public String nombre, apellido;
+
+    //Crear los metodos Get y Set para poder acceder a nuestros atributos de tipo privado
+    void setid (int id) {
+        this.id=id;
+    }
+    int getid(){
+        return id;
+    }
+    //Crear el metodo para mostrar los datos
+    void verDatos(){
+        System.out.println("id: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
---
+++ Encapsulamiento/Resultados.java
@@ -0,0 +1,13 @@
+package Encapsulamiento;
+
+public class Resultados {
+    public static void main(String[] args) {
+        Estudiante estu1=new Estudiante();
+        Estudiante estu2=new Estudiante();
+        estu1.setid(2003);
+        estu1.nombre="Miguel";
+        estu1.apellido="Ugalde Guzman";
+        estu1.verDatos();
+    }
+    
+}
